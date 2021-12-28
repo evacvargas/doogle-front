e2353550cdc7fc293d66023c514ab3cb25bcb13c
eva vargas <evargasabg@gmail.com>
2021-12-27 23:01:10 -0400

    Add BASE_URL constant

--- src/actions/index.actions.js
+++ src/actions/index.actions.js
@@ -1,91 +1,112 @@
-import axios from 'axios';
-import { get_breeds, get_all_tempers, filter_by_tempers, filter_by_breed_created, get_detail_breed, dogs_by_name, sort_by_weight, sort_by_breed } from './actions.types';
+import axios from "axios";
+import {
+  get_breeds,
+  get_all_tempers,
+  filter_by_tempers,
+  filter_by_breed_created,
+  get_detail_breed,
+  dogs_by_name,
+  sort_by_weight,
+  sort_by_breed,
+} from "./actions.types";
+
+const BASE_URL = "https://dry-beyond-88239.herokuapp.com";
 
 export const getBreeds = function (params) {
   return async function (dispatch) {
     try {
-      let result = await axios.get('http://localhost:3002/dogs');
+      let result = await axios.get(`${BASE_URL}/dogs`);
       return dispatch({
         type: get_breeds,
-        payload: result.data
-      })
-    } catch (error) { return error }
-  }
+        payload: result.data,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
 };
 
 export const getAllTempers = function (payload) {
   return async function (dispatch) {
     try {
-      let result = await axios.get('http://localhost:3002/temperament');
+      let result = await axios.get(`${BASE_URL}/temperament`);
       return dispatch({
         type: get_all_tempers,
-        payload: result.data
-      })
-    } catch (error) { return error }
-  }
-}
+        payload: result.data,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
+};
 
 export const filterByTempers = function (payload) {
   return {
     type: filter_by_tempers,
-    payload
-  }
-}
+    payload,
+  };
+};
 
 export const filterByBreedCreated = function (payload) {
   return {
     type: filter_by_breed_created,
-    payload
-  }
-}
+    payload,
+  };
+};
 
 export const sortByWeight = function (payload) {
   return {
     type: sort_by_weight,
-    payload
-  }
-}
+    payload,
+  };
+};
 
 export const sortByBreed = function (payload) {
   return {
     type: sort_by_breed,
-    payload
-  }
-}
+    payload,
+  };
+};
 
 export const createBreed = function (payload) {
   return async function (dispatch) {
-    payload.weight = payload.minWeight + ' - ' + payload.maxWeight;
-    payload.height = payload.minHeight + ' - ' + payload.maxHeight;
-    
+    payload.weight = payload.minWeight + " - " + payload.maxWeight;
+    payload.height = payload.minHeight + " - " + payload.maxHeight;
+
     try {
-      let result = await axios.post('http://localhost:3002/dog', payload)
+      let result = await axios.post(`${BASE_URL}/dog`, payload);
       return result;
-    } catch (error) { return error }
-  }
-}
+    } catch (error) {
+      return error;
+    }
+  };
+};
 
 export const getDetailBreed = function (id) {
   return async function (dispatch) {
     try {
-      let result = await axios.get('http://localhost:3002/dogs/' + id);
+      let result = await axios.get(`${BASE_URL}/dogs/` + id);
       return dispatch({
         type: get_detail_breed,
-        payload: result.data
-      })
-    } catch (error) { return error }
-  }
-}
+        payload: result.data,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
+};
 
-export const getDogsByName = function (payload) { //un filtrado mas
+export const getDogsByName = function (payload) {
+  //un filtrado mas
   return async function (dispatch) {
     try {
-      let result = await axios.get('http://localhost:3002/dogs?name=' + payload);
+      let result = await axios.get(`${BASE_URL}/dogs?name=${payload}`);
       return dispatch({
         type: dogs_by_name,
-        payload: result.data
-      })
-    } catch (error) { return error }
-  }
-}
-
+        payload: result.data,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
-import { store } from './store/index.store';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./store/index.store";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -15,7 +15,7 @@ ReactDOM.render(
       </React.StrictMode>
     </Provider>
   </BrowserRouter>,
-    document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
